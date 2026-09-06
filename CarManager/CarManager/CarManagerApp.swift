6429254775ccebfe261fae0af8fb29b05c293780
Ivan <ivanznak04@gmail.com>
2026-09-06 21:29:33 +0200

    Initial commit

---
+++ CarManager/CarManager/CarManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarManagerApp.swift
+//  CarManager
+//
+//  Created by Ivan on 06/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct CarManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
